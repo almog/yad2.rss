a818b37710bcc5d2448af744d1eeec5d5a3b05d7
Daniel.Kalman <daniel.kalman@klarna.com>
2013-07-14 16:46:31 +0300

    Support yad2 query customization from url

--- apartment_search.rb
+++ apartment_search.rb
@@ -10,7 +10,7 @@ class ApartmentSearch < Sinatra::Base
   end
 
   get "/yad2.rss" do
-    @apartments = load_apartments
+    @apartments = load_apartments(request.params)
     headers 'Content-Type' => 'text/xml; charset=windows-1255'
     builder :rss
   end
@@ -19,10 +19,11 @@ class ApartmentSearch < Sinatra::Base
   def self.url(until_price=2300,from_square_meter=70,until_square_meter=95,until_rooms=4,from_rooms=3,until_floor=3) "http://www.yad2.co.il/Nadlan/rent.php?AreaID=22&SubAreaID=0&HomeTypeID=&fromRooms=#{from_rooms}&untilRooms=#{until_rooms}&FromFloor=&Floor=#{until_floor}&fromSquareMeter=#{from_square_meter}&untilSquareMeter=#{until_square_meter}&fromPrice=&untilPrice=#{until_price}&PriceType=1&fromSquareMeter=#{from_square_meter}&untilSquareMeter=#{until_square_meter}&Furniture=1&Parking=1&AirConditioner=1&FreeText=&ad_sr=1"
   end
 
-  def load_apartments
+  def load_apartments(request_params)
     agent = Mechanize.new { |a| a.log = Logger.new('apartment.log') }
     agent.user_agent_alias = "Mac Mozilla"
-    page = agent.get(ApartmentSearch.url)
+    url = create_url(request_params)
+    page = agent.get(url)
     trs = page.search('//div[@id="main_table"]//tr[@class="ActiveLink"]')
     trs.map do |tr|
       cells = tr / "td"
@@ -40,6 +41,10 @@ class ApartmentSearch < Sinatra::Base
   rescue => e
     agent.log.warn "an error (#{e}) occured on #{ApartmentSearch.url}"
     raise e
+  end
+
+  def create_url(params)
+
   end
 
 end
