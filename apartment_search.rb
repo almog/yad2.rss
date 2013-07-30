bd5e3b7711b72ff88192906582ec55eb0eae07f0
Almog Kurtser <almog.kurtser@klarna.com>
2013-07-30 15:29:22 +0300

    Disable images loading for Poltergeist

--- apartment_search.rb
+++ apartment_search.rb
@@ -1,6 +1,11 @@
 Bundler.require(:default)
 require "addressable/uri"
 require 'capybara/poltergeist'
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, phantomjs_options: ["--load-images=false"])
+end
+
 Capybara.javascript_driver = :poltergeist
 Capybara.current_driver = :poltergeist
 
